c2e5f174301b927a0db0477f20b0247a7d5d71d1
TamKha <83918331+TamKha@users.noreply.github.com>
2021-11-23 22:33:14 +0100

    Demo1
    ...

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, Image } from 'react-native';
 
 export default function App() {
+  var TextInputValue = "<answer goes here>";
+  const handlePress = () => console.log(TextInputValue)
+  const handleChangeText = () => console.log (TextInputValue)
   return (
+    
     <View style={styles.container}>
       
       <StatusBar style="auto" />
       
-      <View style={{ width: "100%", height: "100%", backgroundColor: "white" }}>
+      
 <View
-style={{ width: "100%", height: "30%", backgroundColor: "lightblue", textAlign:"center" }}> 
- <text style={{ fontWeight:"bolder", fontSize:"30px"}}>
-      Hello World! </text>
+style={{ width: "100%", height: "30%", backgroundColor: "lightblue", textAlign:"center"}}> 
+ <h1 style={{ fontWeight:"bolder", fontSize:"35px"}}>
+      Hello World! </h1>
 
-      <text style={{fontWeight:"normal", fontSize:"20px", fontWeight:"bold", textAlign:"center"}}>
+      <text style={{fontSize:"20px", fontWeight:"bold"}}>
         First React Native App
+        <p style={{fontWeight:"normal", fontSize:"15px"}}>This is a paragraph explaining what the app is all about!</p>
       </text>
       
+      
     </View>
-</View>
+    <text style = {{fontWeight:"bold", margin:"20px"}}>
+      What would you like to get out of this course?
+    </text>
 
-    </View>
+    <TextInput style ={{backgroundColor: "lightblue", width:"400px" , height:"120px", textAlign:"center", margin:"5px"}}placeholder="initial"  onChangeText={handleChangeText}/>
+
+    <text style={{fontWeight:"bold" , margin:"20px"}}>An emoji describing how you are feeling about the course:</text>
+
+    <Image style={{width:150, height:150, marginBottom:"15px"}} source={require('./assets/icon.png')}/>
+
+
+    <Button title = "submit" onPress = {handlePress} color = "darkblue"/>
 
     
+</View>
+
   );
 }
 
@@ -32,6 +49,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "white",
     alignItems: 'center',
-    justifyContent: 'center',
+  
   },
 });
